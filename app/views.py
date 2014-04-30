7194b39ffe0717b0c6fb4842db720cf18bea4139
amygdalama <amyehanlon@gmail.com>
2014-04-29 21:32:48 -0400

    add control flow in index template for title and posts

--- app/views.py
+++ app/views.py
@@ -5,4 +5,17 @@ from app import app
 @app.route('/index')
 def index():
     user = {"nickname" : "Amy"}
-    return render_template("index.html", title="Home", user=user)
+    posts = [
+        {
+            'author' : {'nickname' : 'John'},
+            'body' : 'Here is some fake content!'
+        },
+        {
+            'author' : {'nickname' : 'Mary'},
+            'body' : 'Really excellent blog post from Mary.'
+        }
+    ]
+    return render_template("index.html", 
+        title="Home", 
+        user=user, 
+        posts=posts)
