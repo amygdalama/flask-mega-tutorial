56b3a10dd1778dc536f565b149ba6a62be6ae8cb
amygdalama <amyehanlon@gmail.com>
2014-04-29 22:20:10 -0400

    switch to openid vs open_id

--- app/forms.py
+++ app/forms.py
@@ -3,5 +3,5 @@ from wtforms import TextField, BooleanField
 from wtforms.validators import Required
 
 class LoginForm(Form):
-    open_id = TextField('open_id', validators=[Required()])
+    openid = TextField('openid', validators=[Required()])
     remember_me = BooleanField('remember_me', default=False)
